c2483e960b7096dec8a6787c335836119a24d20e
solu <sendsoluex@gmail.com>
2014-04-13 21:56:09 +0800

    1.20

---
+++ ex1/gcd.rkt
@@ -0,0 +1,35 @@
+#lang racket
+(define (gcd a b)
+  (if (= b 0)
+      a
+      (gcd b (remainder a b))))
+
+;;;最大公约数
+;;normal-order evaluation
+;(gcd 206 40)
+;(if (= 40 0) ...)
+;(gcd 40 (remainder 206 40))
+;(if (= (remainder 206 40) 0) ...)
+;(if (= 6 0) ...)
+;(gcd (remainder 206 40) (remainder 40 (remainder 206 40)))
+;(if (= (remainder 40 (remainder 206 40)) 0) ..)
+;(if (= 4 0) ...)
+;(gcd (remainder 40 (remainder 206 40)) (remainder (remainder 206 40) (remainder 40 (remainder 206 40))))
+;(if (= (remainder (remainder 206 40) (remainder 40 (remainder 206 40))) 0) ...)
+;(if (= 2 0) ...)
+;(gcd (remainder (remainder 206 40) (remainder 40 (remainder 206 40))) (remainder (remainder 40 (remainder 206 40)) (remainder (remainder 206 40) (remainder 40 (remainder 206 40))))) 
+;(if (= (remainder (remainder 40 (remainder 206 40)) (remainder (remainder 206 40) (remainder 40 (remainder 206 40)))) 0) ...) 
+;(if (= 0 0) ...)
+;(remainder (remainder 206 40) (remainder 40 (remainder 206 40)))
+
+;;applicative-order evaluation
+;(gcd 206 40)
+;(gcd 40 (remainder 206 40))
+;(gcd 40 6)
+;(gcd 6 (remainder 40 6))
+;(gcd 6 4)
+;(gcd 4 (remainder 6 4))
+;(gcd 4 2)
+;(gcd 2 (remainder 4 2))
+;(gcd 2 0)
+;2
